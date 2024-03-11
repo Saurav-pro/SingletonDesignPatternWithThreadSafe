3c7deed8b0f84fa9a9ce4158c2363576a68a29db
Saurav <Saurav@DESKTOP-TQKCOQ4>
2024-03-11 14:23:01 +0530

    Add project files.

---
+++ SingletonDesignPatternWithThreadSafe/Program.cs
@@ -0,0 +1,17 @@
+﻿using SingletonDesignPatternWithThreadSafe;
+
+Thread thread1 = new Thread(() => 
+{
+    var Programinstance1 =    UploadService.Instance(1);
+
+});
+Thread thread2 = new Thread(() =>
+{
+    var Programinstance2 = UploadService.Instance(2);
+
+});
+
+thread1.Start();
+thread2.Start();
+thread1.Join(); 
+thread2.Join();
---
+++ SingletonDesignPatternWithThreadSafe/UploadService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPatternWithThreadSafe
+{
+    public class UploadService
+    {
+        private UploadService(){}
+
+        private static UploadService _instance;
+        public int id { get; private set; }
+
+        private static readonly object _instanceLock = new object();
+        public static UploadService Instance(int id)
+        {
+            
+            if (_instance == null)
+            {
+                lock (_instanceLock) 
+                {
+                    _instance = new UploadService();
+                    _instance.id = id;
+                }
+            }
+            return _instance;
+        }
+
+    }
+}
